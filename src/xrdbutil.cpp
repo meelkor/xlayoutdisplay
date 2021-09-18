f870257dd31e590526c3d751182ce1ff88617bd9
Jan Seda <meelkor11@gmail.com>
2021-09-18 18:55:48 +0200

    feat: do not touch dpi if -1 provided
    If the DPI is controlled by different script, we do not want this
    layouting script to change it. Now -1 can be provided in the -i switch
    to prevent that.

--- src/xrdbutil.cpp
+++ src/xrdbutil.cpp
@@ -23,8 +23,12 @@ using namespace std;
 
 const std::string renderXrdbCmd(const long &dpi) {
     stringstream ss;
-    ss << "echo \"Xft.dpi: "
-       << dpi
-       << "\" | xrdb -merge";
+
+    if (dpi != -1) {
+        ss << "echo \"Xft.dpi: "
+            << dpi
+            << "\" | xrdb -merge";
+    }
+
     return ss.str();
 }
